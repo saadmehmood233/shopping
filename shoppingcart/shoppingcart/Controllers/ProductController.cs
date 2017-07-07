1393ed35dfec1e5b878207e7e94b5733f8711cf5
saadmehmood233 <saadmehmood.me@gmail.com>
2017-07-07 03:26:48 -0700

    Created Home Page

---
+++ shoppingcart/shoppingcart/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using shoppingcart.Models;
+using PagedList;
+using PagedList.Mvc;
+
+namespace shoppingcart.Controllers
+{
+    public class ProductController : Controller
+    {
+        private shoppingcartEntities db = new shoppingcartEntities();
+        public ActionResult Index(string category = "All", int page = 1, int pageSize = 8)
+        {
+            Category cat = db.Categories.Where(x=>x.name == category).FirstOrDefault();
+            List<Category> categories = db.Categories.ToList();
+
+            List<Product> products = null;
+            if (cat == null)
+                products = db.Products.ToList();
+            else
+                products = db.Products.Where(x=>x.category_id == cat.id).ToList();
+
+            PagedList<Product> pagedProduct = new PagedList<Product>(products, page, pageSize);
+
+            CategoriesProducts model = new CategoriesProducts
+            {
+                activeCategory = category,
+                categories = categories,
+                products = pagedProduct
+            };
+
+            return View(model);
+        }
+	}
+}
---
+++ shoppingcart/shoppingcart/Models/CategoriesProducts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace shoppingcart.Models
+{
+    public class CategoriesProducts
+    {
+        public string activeCategory { get; set; }
+        public List<Category> categories { get; set; }
+        public PagedList.IPagedList<Product> products { get; set; }
+    }
+}
