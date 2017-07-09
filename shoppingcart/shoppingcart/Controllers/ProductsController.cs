d636337b6bbfe3cd83896a730e3a81b8fa7d2d60
saadmehmood233 <saadmehmood.me@gmail.com>
2017-07-09 05:50:45 -0700

    Added Products CRUD Funcationality

--- shoppingcart/shoppingcart/Controllers/ProductsController.cs
+++ shoppingcart/shoppingcart/Controllers/ProductsController.cs
@@ -80,20 +80,25 @@ namespace shoppingcart.Controllers
             return View(product);
         }
 
-        // POST: /Products/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="id,name,description,price,img_path,category_id")] Product product)
+        public ActionResult Edit(Product product, HttpPostedFileBase file)
         {
-            if (ModelState.IsValid)
+            if (file != null)
             {
-                db.Entry(product).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(product);
+                var fileName = Guid.NewGuid().ToString() +
+                   System.IO.Path.GetExtension(file.FileName);
+
+                var uploadUrl = Server.MapPath("~/images");
+                string path = Path.Combine(uploadUrl, fileName);
+
+                file.SaveAs(path);
+
+                product.img_path = path;
+            } 
+            db.Entry(product).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         // GET: /Products/Delete/5
